9c2a4f469ebd0105d0a8d2173ba25dbdffe10471
Gustavo Henrique <gustavo.depaulo@appsistemas.com.br>
2024-02-26 23:20:08 -0300

    Add project files.

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿var _random = new Random();
+const string _chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+const string _specialChars = "!@#$%^&*()";
+
+string GeneratePassword(int length, bool includeSpecialChars)
+{
+    var chars = includeSpecialChars ? _chars + _specialChars : _chars;
+
+    return new string(Enumerable.Repeat(chars, length).Select(x => x[_random.Next(x.Length)]).ToArray());
+}
+
+Console.WriteLine(GeneratePassword(8, true));
